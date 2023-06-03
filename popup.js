5434b9f667ec08593a7cb012695c898d0d5ae83f
David León V <datevid@gmail.com>
2023-06-02 20:34:57 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {action: 'search'}, function(response) {
                 document.getElementById('result').innerHTML = response.count + ' ocurrencias encontradas';
-                console.log("asdf")
+                console.log("asdfasdfasdfsdf")
             });
         });
     });
